eb5afd428be507e8f0cf2511cbe4b9bc10354c00
tkymsys <matsumaet@tkymsys.com>
2020-05-05 20:48:11 +0900

    記録

--- docker-file/setup.sh
+++ docker-file/setup.sh
@@ -1,36 +1,33 @@
 #!/bin/bash
 
-STEAM_DIR="/home/${SERVER_UNAME}/Steam"
-SERVER_DIR="${STEAM_DIR}/steamapps/common/Left 4 Dead 2 Dedicated Server"
+STEAMCMD_DIR="/home/${SERVER_UNAME}/steamcmd"
+SERVER_DIR="${STEAMCMD_DIR}/steamapps/common/Left 4 Dead 2 Dedicated Server"
+WORK_DIR="/home/${SERVER_UNAME}/work"
 
-mkdir ${STEAM_DIR}
-
-# SteamCMDのインストール
-cd ${STEAM_DIR}
-curl -sqL "https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz" | tar zxvf -
+mkdir "${WORK_DIR}"
 
 # サーバーセットアップ
-cd ${STEAM_DIR}
+cd ${STEAMCMD_DIR}
 ./steamcmd.sh +login anonymous +app_update 222860 +quit
 cp -p /tmp/server.cfg "${SERVER_DIR}/left4dead2/cfg"
-ln -s steamcmd/linux32/steamclient.so ~/.steam/sdk32/steamclient.so
+# ln -s steamcmd/linux32/steamclient.so ~/.steam/sdk32/steamclient.so
 
 # Metamodのインストール
-cd > /dev/null
+cd ${WORK_DIR} > /dev/null
 mkdir metamod
 cd metamod > /dev/null
 wget https://mms.alliedmods.net/mmsdrop/1.10/mmsource-1.10.7-git971-linux.tar.gz
 tar xf mmsource-1.10.7-git971-linux.tar.gz
-chmod -R 755 *
+chmod -R +rx *
 cp -rf addons "${SERVER_DIR}/left4dead2"
 
 # SourceModのインストール
-cd > /dev/null
+cd ${WORK_DIR} > /dev/null
 mkdir sourcemod
 cd sourcemod > /dev/null
 wget https://sm.alliedmods.net/smdrop/1.11/sourcemod-1.11.0-git6508-linux.tar.gz
 tar xf sourcemod-1.11.0-git6508-linux.tar.gz
-chmod -R 755 *
+chmod -R +rx *
 cp -rf addons "${SERVER_DIR}/left4dead2"
 # cp -rfp cfg "${SERVER_DIR}/left4dead2"
 
--- sub-script/build-image.sh
+++ sub-script/build-image.sh
@@ -1,16 +1,26 @@
 #/bin/bash
 
+function printBuildArg () {
+  for line in `cat ${1}`
+  do
+    key=`echo ${line} | awk -F"=" '{print $1}'`
+    value=`echo ${line} | awk -F"=" '{print $2}'`
+    echo -n " --build-arg ${key}=${value}"
+  done
+}
+
+
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 cd ${SCRIPT_DIR} > /dev/null
 
 . ../docker-file/.env
-. ./common.sh
 
-buildArg=`createBuildArg "${SCRIPT_DIR}/../docker-file/.env"`
-buildArgPUID="--build-arg PUID=${SERVER_UID}"
+buildArg=`printBuildArg "${SCRIPT_DIR}/../docker-file/.env"`
+Dokcerfile_steamcmd=`wget -q -O - "https://raw.githubusercontent.com/CM2Walki/steamcmd/eb70cee29a2a2ed44596f1e305f92abc365209bf/buster/Dockerfile"`
 
 cd ../docker-file > /dev/null
-docker build -t l4d2server_base:${IMAGE_VERSION}  ${buildArg} ${buildArgPUID} -f Dockerfile_base  .
-# docker build -t l4d2server_setup:${IMAGE_VERSION} ${buildArg} -f Dockerfile_setup .
-# docker build -t l4d2server_boot:${IMAGE_VERSION}  ${buildArg} -f Dockerfile_boot  .
+echo "${Dokcerfile_steamcmd}" | docker build -t l4d2server_steamcmd:${IMAGE_VERSION} --build-arg PUID=${SERVER_UID} -
+docker build -t l4d2server_base:${IMAGE_VERSION}  ${buildArg} -f Dockerfile_base  .
+docker build -t l4d2server_setup:${IMAGE_VERSION} ${buildArg} -f Dockerfile_setup .
+docker build -t l4d2server_boot:${IMAGE_VERSION}  ${buildArg} -f Dockerfile_boot  .
 cd - > /dev/null
